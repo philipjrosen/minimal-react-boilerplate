ed44b412c719a193a1b6c207ee019bd8c93458be
Philip Rosen <philipjrosen@gmail.com>
2018-03-11 14:12:10 -0700

    add react and react-dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Here comes the sun...');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Minimal React + Webpack + Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
